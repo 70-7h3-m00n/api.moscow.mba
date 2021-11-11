17801f0065cafcf8578d0cc724c578a5c4ec63a1
Novailoveyou <novailoveyoux@gmail.com>
2021-11-11 16:04:27 +0300

    added getStaticPaths route

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -160,5 +160,57 @@ module.exports = {
       .exec()
 
     return program.filter(item => item.category?.type === type)[0]
+  },
+  getStaticPaths: async ctx => {
+    const type = ctx.request.url.split('/')[3]
+
+    const paths = await strapi
+      .query('product')
+      .model.find(
+        {},
+        {
+          // slug: 1,
+          // studyFormat: 1,
+          // category: 1,
+          id: 0,
+          title: 0,
+          whatWillYouLearn: 0,
+          study_field: 0,
+          specializedSubjects: 0,
+          duration: 0,
+          baseSubjects: 0,
+          programModulesCounters: 0,
+          diplomas: 0,
+          whoIsFor: 0,
+          specializedSubjectsAddons: 0,
+          picture: 0,
+          subjectsStickerType: 0,
+          localizations: 0,
+          id: 0,
+          published_at: 0,
+          locale: 0,
+          goal: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+          created_by: 0,
+          updated_by: 0,
+          showInMenu: 0,
+          copyToKk: 0,
+          description: 0,
+          price: 0,
+          discount: 0
+        }
+      )
+      .populate([
+        {
+          path: 'category',
+          select: 'type slug',
+          match: { type: { $eq: type } }
+        }
+      ])
+      .exec()
+
+    return paths
   }
 }
