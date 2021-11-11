60ee20bf5ea67ea4cfefd270dec9ada43259db38
Novailoveyou <novailoveyoux@gmail.com>
2021-11-11 12:29:00 +0300

    added custom route and method to product controller to query less data

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,52 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  findReduced: async () => {
+    // const product = await strapi
+    //   .query('product')
+    //   .find({ _limit: -1 })
+    //   .select({
+    //     id: 1,
+    //     title: 1,
+    //     slug: 1,
+    //     studyFormat: 1
+    //   })
+    //   .populate('category.slug', 'category.type')
+    // console.log(product)
+    // const output = await strapi.query('product').model.find({}).select({
+    //   id: 1,
+    //   title: 1,
+    //   slug: 1,
+    //   studyFormat: 1,
+    //   category: 1
+    // })
+    // console.log(product2)
+    const programs = await strapi
+      .query('product')
+      .model.find({})
+      .select(['id', 'title', 'slug', 'studyFormat', 'category'])
+      .populate('category')
+
+    // const output = programs.filter(program => program).map(program => {
+    //   console.log(program)
+    //   return {
+    //     id: program.id,
+    //     title: program.title,
+    //     slug: program.slug,
+    //     studyFormat: program.studyFormat,
+    //     category: {
+    //       id: program.category.id,
+    //       type: program.category.type,
+    //       slug: program.category.slug
+    //     }
+    //   }
+    // })
+
+    return programs
+  }
+}
