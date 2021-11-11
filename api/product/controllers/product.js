32bf2f5a8c29c41cafbdca792396de0bb3bfd78d
Novailoveyou <novailoveyoux@gmail.com>
2021-11-11 15:53:47 +0300

    added getProgram route

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -17,6 +17,7 @@ module.exports = {
           // slug: 1,
           // studyFormat: 1,
           // category: 1,
+          study_field: 0,
           whatWillYouLearn: 0,
           specializedSubjects: 0,
           duration: 0,
@@ -48,6 +49,116 @@ module.exports = {
 
     return programs
   },
-  getStaticPropsProfession: {},
-  getStaticPropsPromo: {}
+  getStaticPropsProfession: async () => {
+    const programs = await strapi
+      .query('product')
+      .model.find(
+        {},
+        {
+          // id: 1,
+          // title: 1,
+          // slug: 1,
+          // studyFormat: 1,
+          // category: 1,
+          // study_field: 1,
+          // duration: 1
+          whatWillYouLearn: 0,
+          specializedSubjects: 0,
+          baseSubjects: 0,
+          programModulesCounters: 0,
+          diplomas: 0,
+          whoIsFor: 0,
+          specializedSubjectsAddons: 0,
+          picture: 0,
+          subjectsStickerType: 0,
+          localizations: 0,
+          id: 0,
+          published_at: 0,
+          locale: 0,
+          goal: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+          created_by: 0,
+          updated_by: 0,
+          showInMenu: 0,
+          copyToKk: 0,
+          description: 0,
+          price: 0,
+          discount: 0
+        }
+      )
+      .populate([
+        { path: 'category', select: 'type slug' },
+        { path: 'study_field', select: 'id name' },
+        { path: 'duration', select: 'minStudyMonths' }
+      ])
+
+    return programs
+  },
+  getStaticPropsPromo: async () => {
+    const programs = await strapi
+      .query('product')
+      .model.find(
+        {},
+        {
+          // id: 1,
+          // title: 1,
+          // slug: 1,
+          // studyFormat: 1,
+          // category: 1,
+          // whatWillYouLearn: 1
+          study_field: 0,
+          specializedSubjects: 0,
+          duration: 0,
+          baseSubjects: 0,
+          programModulesCounters: 0,
+          diplomas: 0,
+          whoIsFor: 0,
+          specializedSubjectsAddons: 0,
+          picture: 0,
+          subjectsStickerType: 0,
+          localizations: 0,
+          id: 0,
+          published_at: 0,
+          locale: 0,
+          goal: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+          created_by: 0,
+          updated_by: 0,
+          showInMenu: 0,
+          copyToKk: 0,
+          description: 0,
+          price: 0,
+          discount: 0
+        }
+      )
+      .populate([
+        { path: 'category', select: 'type slug' },
+        { path: 'whatWillYouLearn' }
+      ])
+
+    return programs
+  },
+  getProgram: async ctx => {
+    const typeSlug = ctx.request.url.split('/')[3].split('.')
+    const type = typeSlug[0]
+    const slug = typeSlug[1]
+
+    const program = await strapi
+      .query('product')
+      .model.find({ slug })
+      .populate([
+        {
+          path: 'category',
+          select: 'type slug',
+          match: { type: { $eq: type } }
+        }
+      ])
+      .exec()
+
+    return program.filter(item => item.category?.type === type)[0]
+  }
 }
