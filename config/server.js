ebc1c4ad04ee795b4c66187d57eabf3344063b01
Novailoveyou <novailoveyoux@gmail.com>
2021-11-15 15:24:25 +0300

    env update

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c6aa9711f0ba774a184db31ec22aaa84'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
