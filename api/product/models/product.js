80928705ee2ceace5162fb2fd28f2f4ca34578bf
Novailoveyou <novailoveyoux@gmail.com>
2022-03-11 15:48:42 +0300

    added handleRevalidate

--- api/product/models/product.js
+++ api/product/models/product.js
@@ -5,48 +5,31 @@
  * to customize this model
  */
 
-// const { createSlug, copy, copyUpdate } = require('../../../helpers')
-// const { programsUrl } = require('../../../config/config')
+// const { handleRevalidate } = require('../../../helpers')
 
-// module.exports = {
-//   lifecycles: {
-//     beforeCreate: async data => {
-//       // const createSlugRes = createSlug({
-//       //   data,
-//       //   field: 'type',
-//       //   slug: 'slug'
-//       // })
-//     },
-//     beforeUpdate: async (params, data) => {
-//       // const createSlugRes = createSlug({
-//       //   data,
-//       //   field: 'type',
-//       //   slug: 'slug'
-//       // })
-//     },
-//     afterCreate: async (result, data) => {
-//       // if (data.locale === 'ru' && data.copyToKk === true) {
-//       //   const copyResKk = await copy({
-//       //     method: 'POST',
-//       //     path: programsUrl,
-//       //     locale: 'kk',
-//       //     body: data,
-//       //     id: result._id
-//       //   })
-//       // }
-//     },
-//     afterUpdate: async (result, params, data) => {
-//       // if (result.locale === 'ru' && result.copyToKk === true) {
-//       //   const copyUpdateResKk = await copyUpdate({
-//       //     method: 'PUT',
-//       //     path: programsUrl,
-//       //     locale: 'kk',
-//       //     body: data,
-//       //     localizations: result.localizations
-//       //   })
-//       // }
-//     }
-//   }
-// }
-
-module.exports = {}
+module.exports = {
+  // lifecycles: {
+  //   // beforeCreate: async data => {
+  //   //   // const createSlugRes = createSlug({
+  //   //   //   data,
+  //   //   //   field: 'type',
+  //   //   //   slug: 'slug'
+  //   //   // })
+  //   // },
+  //   // beforeUpdate: async (params, data) => {
+  //   //   // const createSlugRes = createSlug({
+  //   //   //   data,
+  //   //   //   field: 'type',
+  //   //   //   slug: 'slug'
+  //   //   // })
+  //   // },
+  //   afterCreate: async (result, data) =>
+  //     await handleRevalidate({
+  //       path: `/programs/${result.category.type}/online/${result.slug}`
+  //     }),
+  //   afterUpdate: async (result, params, data) =>
+  //     await handleRevalidate({
+  //       path: `/programs/${result.category.type}/online/${result.slug}`
+  //     })
+  // }
+}
--- api/teacher/models/teacher.js
+++ api/teacher/models/teacher.js
@@ -5,58 +5,31 @@
  * to customize this model
  */
 
-// const { createSlug, copy, copyUpdate } = require('../../../helpers')
-// const { teachersUrl } = require('../../../config/config')
+// const { handleRevalidate } = require('../../../helpers')
 
-// module.exports = {
-//   lifecycles: {
-//     beforeCreate: async data => {
-//       // const createSlugRes = createSlug({
-//       //   data,
-//       //   field: 'name',
-//       //   slug: 'slug'
-//       // })
-//     },
-//     beforeUpdate: async (params, data) => {
-//       // const createSlugRes = createSlug({
-//       //   data,
-//       //   field: 'name',
-//       //   slug: 'slug'
-//       // })
-//     },
-//     afterCreate: async (result, data) => {
-//       // console.log('result')
-//       // console.log(JSON.stringify(result))
-//       // console.log('data')
-//       // console.log(JSON.stringify(data))
-//       // if (data.locale === 'ru' && data.copyToKk === true) {
-//       //   const copyResKk = await copy({
-//       //     method: 'POST',
-//       //     path: teachersUrl,
-//       //     locale: 'kk',
-//       //     body: data,
-//       //     id: result._id
-//       //   })
-//       // }
-//     },
-//     afterUpdate: async (result, params, data) => {
-//       // console.log('result')
-//       // console.log(JSON.stringify(result))
-//       // console.log('params')
-//       // console.log(JSON.stringify(params))
-//       // console.log('data')
-//       // console.log(JSON.stringify(data))
-//       // if (result.locale === 'ru' && result.copyToKk === true) {
-//       //   const copyUpdateResKk = await copyUpdate({
-//       //     method: 'PUT',
-//       //     path: teachersUrl,
-//       //     locale: 'kk',
-//       //     body: data,
-//       //     localizations: result.localizations
-//       //   })
-//       // }
-//     }
-//   }
-// }
-
-module.exports = {}
+module.exports = {
+  // lifecycles: {
+  //   beforeCreate: async data => {
+  //     // const createSlugRes = createSlug({
+  //     //   data,
+  //     //   field: 'name',
+  //     //   slug: 'slug'
+  //     // })
+  //   },
+  //   beforeUpdate: async (params, data) => {
+  //     // const createSlugRes = createSlug({
+  //     //   data,
+  //     //   field: 'name',
+  //     //   slug: 'slug'
+  //     // })
+  //   },
+  //   afterCreate: async (result, data) =>
+  //     await handleRevalidate({
+  //       path: `/teachers/${result.slug}`
+  //     }),
+  //   afterUpdate: async (result, params, data) =>
+  //     await handleRevalidate({
+  //       path: `/teachers/${result.slug}`
+  //     })
+  // }
+}
